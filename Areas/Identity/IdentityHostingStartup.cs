2840e4848e8f59be6c41e888e40a7fcd7b8e3a25
lukas ferreira <contatolukasferreira@gmail.com>
2021-04-04 22:14:25 -0300

    New Firstpage, Override Identy Login and Register classes

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StuffAndThings.Data;
+
+[assembly: HostingStartup(typeof(StuffAndThings.Areas.Identity.IdentityHostingStartup))]
+namespace StuffAndThings.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
