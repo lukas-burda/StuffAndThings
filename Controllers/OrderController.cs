a412434ad5f54fb0441dd93a948f4af9c2cace09
lukas ferreira <contatolukasferreira@gmail.com>
2021-04-27 21:42:03 -0300

    Order  - Model controller e views

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -12,8 +12,8 @@ namespace StuffAndThings.Controllers
         public IActionResult Index()
         {
             DBContext _context = new DBContext();
-            List <UserModel> users = _context.Users.ToList();
-            return View();
+            List <OrderModel> users = _context.Orders.ToList();
+            return View(users);
         }
 
         public IActionResult Create()
@@ -29,10 +29,26 @@ namespace StuffAndThings.Controllers
             return View(User);
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(OrderModel order)
         {
-            
-            return View();
+            DBContext _context = new DBContext();
+            if (order.Id == new Guid())
+            {
+                order.Id = Guid.NewGuid();
+                order.FriendlyCode = "S&T-" + OrderFriendlyCodeGenerator(4);
+                order.Status = Enums.OrderStatus.Created;
+                order.CreateDate = DateTime.UtcNow;
+                order.LastUpdate = DateTime.UtcNow;
+                order.SubTotal = order.Total - order.Discount;
+                _context.Orders.Add(order);
+            }
+            else
+            {
+                order.LastUpdate = DateTime.Today;
+                _context.Orders.Update(order);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Delete(Guid Id)
@@ -42,6 +58,14 @@ namespace StuffAndThings.Controllers
             _context.Users.Remove(User);
             _context.SaveChanges();
             return View();
+        }
+
+        private static Random FriendlyCode = new Random();
+        public static string OrderFriendlyCodeGenerator(int lenght)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            return new string(Enumerable.Repeat(chars, lenght).Select(s => s[FriendlyCode.Next(s.Length)]).ToArray());
         }
 
     }
--- Models/OrderModel.cs
+++ Models/OrderModel.cs
@@ -12,11 +12,15 @@ namespace StuffAndThings.Models
         public string FriendlyCode { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdate { get; set; }
+        public double Total { get; set; } = 0;
         public double SubTotal { get; set; } = 0;
         public double Discount { get; set; } = 0;
         public string Message { get; set; }
         public UserModel Seller { get; set; }
         public List<SkuModel> Items { get; set; }
         public OrderStatus Status { get; set; } = 0;
+
     }
+
+
 }
