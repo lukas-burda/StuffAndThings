9eafaa8cb258a762f149286f60e7e10937140d8e
lukasferreira <contatolukasferreira@gmail.com>
2021-05-01 16:47:26 -0300

    Commit que nao vai mudar muito (erro na relacao User e Order)

--- Data/Entities/OrderEntity.cs
+++ Data/Entities/OrderEntity.cs
@@ -2,6 +2,7 @@
 using StuffAndThings.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,15 +13,19 @@ namespace StuffAndThings.Data.Entities
         public Guid Id { get; set; }
         public string FriendlyCode { get; set; }
         public DateTime CreateDate { get; set; }
-        public DateTime LastUpdate { get; set; }
         public double Total { get; set; }
         public double SubTotal { get; set; }
         public double Discount { get; set; }
         public string Message { get; set; }
-        public UserEntity OrderPerson { get; set; }
         public List<SkuEntity> Items { get; set; }
         public OrderStatus Status { get; set; } = 0;
         public OrderType Type { get; internal set; } = 0;
+        [ForeignKey("BuyerId")]
+        public virtual UserEntity Buyer { get; internal set; }
+        [ForeignKey("SellerId")]
+        public virtual UserEntity Seller { get; set; }
+        public Guid BuyerId { get; set; }
+        public Guid SellerId { get; set; }
 
         public OrderEntity()
         {
