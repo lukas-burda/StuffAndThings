820dd059ee6da3678262f9e27b6587ed5c4cf1e7
lukasferreira <contatolukasferreira@gmail.com>
2021-05-01 17:31:23 -0300

    Enum Discriminator para Usuario

--- Data/Entities/UserEntity.cs
+++ Data/Entities/UserEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StuffAndThings.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace StuffAndThings.Data.Entities
         public string FullName { get; set; }
         public string CPF { get; set; }
         public string CNPJ { get; set; }
-        public string Discriminator { get; set; }
+        public Discriminator Discriminator { get; set; }
         public string Gender { get; set; }
     }
 }
---
+++ Models/Enums/Discriminator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffAndThings.Models.Enums
+{
+    public enum Discriminator
+    {
+       Seller = 0,
+       Buyer = 1
+
+    }
+}
