ec6a552474bba776e4b57aa7e42cce71871a314b
lukas ferreira <contatolukasferreira@gmail.com>
2021-05-20 21:30:06 -0300

    Showcase add

---
+++ Controllers/ShowcaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StuffAndThings.Controllers
+{
+    public class ShowcaseController : Controller
+    {
+        
+        
+        public IActionResult Index()
+        {
+            DBContext
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Showcase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffAndThings.Models
+{
+    public class Showcase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate{ get; set; }
+        public DateTime EndDate { get; set; }
+        public List<ProductModel> products { get; set; }
+        public DateTime LastUpdatae { get; set; }
+        public Showcase()
+        {
+            products = new List<ProductModel>();
+        }
+        
+
+    }
+}
