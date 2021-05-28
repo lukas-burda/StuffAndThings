fe6fc8ef79559dedafa6791068a4db7b82bff6a9
Matheus Dijan <matheusdijan7@gmail.com>
2021-05-27 21:10:28 -0300

    Adicionado getAllShowcase

--- Controllers/ShowcaseController.cs
+++ Controllers/ShowcaseController.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StuffAndThings.Data;
+using StuffAndThings.Data.Mapper;
 using StuffAndThings.Models;
 
 namespace StuffAndThings.Controllers
 {
     public class ShowcaseController : Controller
     {
-        
-        
-        public IActionResult Index()
+        public static List<ShowcaseModel> GetAllShowcases()
         {
             DBContext _context = new DBContext();
-            //ShowcaseModel sc = _context
-            return View();
+            List<ShowcaseModel> showcases = ShowCaseMapper.Mapper(_context.ShowCases.ToList());
+            showcases.ForEach(x => x.ShowcaseProducts = ShowCaseProductsMapper.Mapper(_context.ShowCaseProducts.Where(y => y.ShowCaseId == x.Id).Include(x => x.Product).ToList()));
+
+            return showcases;
         }
-
-
     }
 }
